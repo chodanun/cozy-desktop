630d881684ea130d131adc81cda6e17143de657c
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2019-01-24 13:33:36 +0100

    fixup! test/world: Demonstrate PouchDB short _rev reuse

--- test/world/pouchdb.js
+++ test/world/pouchdb.js
@@ -15,11 +15,21 @@ describe('PouchDB', () => {
 
   it('assigns _rev to new doc depending on previously deleted ones', async function () {
     const builders = new Builders({pouch: this.pouch})
-    const doc1 = await builders.metafile().create()
-    const doc2 = await builders.metafile(doc1).whateverChange().create()
-    await this.pouch.db.remove(doc2)
-    const doc3 = await builders.metafile().path(doc1.path).create()
 
+    const doc1 = await builders.metafile().create()
+    should(doc1._rev).startWith('1-')
+
+    const doc2 = await builders.metafile(doc1).whateverChange().create()
+    should(doc2._rev).startWith('2-')
+
+    await this.pouch.db.remove(doc2)
+
+    const doc3 = await builders.metafile().path(doc1.path).create()
     should(doc3._rev).startWith('4-')
+
+    // await this.pouch.db.remove(doc3)
+
+    const doc4 = await builders.metafile().path(doc1.path).rev('0').create({force: true})
+    should(doc4._rev).startWith('1-')
   })
 })
