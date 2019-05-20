7120405db1fd075cd97c11e7c935658a05774510
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2019-05-20 10:41:02 +0200

    test/scenarios: Use new disabled syntax
    Fixing prettier when needed.

--- test/scenarios/rename_identical/scenario.js
+++ test/scenarios/rename_identical/scenario.js
@@ -5,6 +5,10 @@
 module.exports = ({
   // FIXME: fails on darwin because cozy-stack uses case-insensitive APFS
   platforms: ['win32'],
+  disabled: {
+    'local/darwin-wtf':
+      'One addDir event is missing, not sure why, possibly a capture issue...'
+  },
   init: [
     { ino: 1, path: 'dir-case/' },
     { ino: 2, path: 'file-case' },
